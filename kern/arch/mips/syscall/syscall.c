1a3ac593a723ddfc8badf45d7dc805f0c88ef51e
Justin Dong <z5209666@ad.unsw.edu.au>
2021-03-31 18:50:43 +1100

    kmalloc struct s

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -144,7 +144,7 @@ syscall(struct trapframe *tf)
 			break;
 
 		case SYS_lseek:
-kprintf("SYS_lseek open: %d\n", (int)tf->tf_a1);
+kprintf("SYS_lseek open: %d +  %d\n", (int)tf->tf_a1, (int)tf->tf_a2);
 			retval = sys_lseek((int)tf->tf_a0, (off_t)tf->tf_a1, (int)tf->tf_a2, &err);
 kprintf("SYS_lseek open: %d\n", (int)retval);
 			break;
